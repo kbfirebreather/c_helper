7f37987f51444f0320e5443b454c583f35313b15
Kyle Bailey <kyle.alexander.bailey@gmail.com>
2015-03-17 07:43:43 -0400

    Update c_helper.h

--- c_helper/c_helper.h
+++ c_helper/c_helper.h
@@ -1,26 +1,107 @@
 #ifndef C_HELPER_H_INCLUDED
 #define C_HELPER_H_INCLUDED
 
-
+// struct for when exploding strings into sub strings
 struct exploded {
   char * string; // pointer to string
-	struct exploded *next;
+  struct exploded *next;
 };
 
 
+/*
+  string replace function
+  @string - input string
+  @sub - string to substitute
+  @replace - string to replace sub with
 
+  Returns substring
+*/
 char * strreplace(char * string, char * sub, char * replace);
 
-int numChar(char * string, char needle); // search string for needle, return num occurances
+/*
+  function to count number of chars in a string
+  @string - input string
+  @neelde - char to look for
 
-char * trim(char * string); // trims leading and trailing spaces in string
+  returns number of found occurances
+*/
+int numChar(char * string, char needle);
 
-char * substr(char * string, int start, int end); // get sub string from start to end
 
-struct exploded * explode(char * string, char * delimeter); // explodes string into pieces, setting each piece to a node in struct exploded
+/*
+  function to trim leading and trailing spaces in a string
+  @string - input string
 
-struct exploded * exploded_add_node(struct exploded * exploded_ptr, char * string); // assists explode(), adds string to exploded_ptr and returns next node in exploded_ptr
+  Returns same string with leading and trailing spaces removed
+*/
+char * trim(char * string);
 
+/*
+  Function to get substring
+  @string - input string
+  @start - starting substring location
+  @end - ending substring location
+
+  Returns formatted string from @start to @end
+*/
+char * substr(char * string, int start, int end);
+
+
+/*
+  Function to find occurance of char in string
+  @string - input string
+  @needle - char to find in string
+  @offset - to find nth @needle in @string
+*/
+int strpos(char * string, char needle, int offset);
+
+
+/*
+  Function to print struct that was used in an explode()
+  Used mainly for debugging purposes 
+
+  @exploded_ptr - pointer to exploded struct variable
+
+  Prints each entry on a new line
+
+  Returns void
+*/
+void print_exploded(struct exploded * exploded_ptr); 
+
+/*
+  Function to split a string by a string
+  This is identical to PHP function explode()
+  @string - input string
+  @delimeter - string to split by
+
+  Returns exploded struct
+*/
+struct exploded * explode(char * string, char * delimeter); 
+
+/*
+  Helper function for explode() to add new nodes based on each delimeter found in explode()
+
+  @exploded_ptr - pointer to exploded struct
+  @string - string to insert into struct
+
+  Returns exploded struct
+*/
+struct exploded * exploded_add_node(struct exploded * exploded_ptr, char * string); 
+
+/*
+  Free memory from explode() function
+  @head - Head node in exploded struct
+
+  Returns void
+*/
 void freeExploded(struct exploded * head);
+
+/*
+  Function to convert a character to an integer
+  @string - string to format to integer
+
+  Returns integer
+*/
+int CharToInt(char * string);
 
 #endif
